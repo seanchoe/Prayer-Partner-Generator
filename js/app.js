afe4b87c331b99e989dce3195224e6304d954ff1
Sean Choe <sean@seanchoe.com>
2016-04-06 21:23:47 -0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var prayerPartnerApp = angular.module('prayerPartnerApp', []);
+
+prayerPartnerApp.controller('GeneratorCtrl', ['$scope', '$location',
+function($scope, $location) {
+  $scope.name = "Test";
+}]);
